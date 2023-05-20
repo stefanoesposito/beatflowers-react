a449eecd1cb65175cd22c328165c4d81429acddc
Stefano Esposito <stefano.esposito@abstract.it>
2023-05-20 11:57:55 +0200

    replaced deprecated userLanguage

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-const userLang = navigator.language || navigator.userLanguage;
+const userLang = navigator.language || navigator.languages[0];
 
 const resources = {
     'en-GB': {
