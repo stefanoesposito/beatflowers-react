a9a7acadce360967d70a53dc5cefe124ea05b281
Stefano Esposito <stefano.esposito@abstract.it>
2023-06-06 15:26:10 +0200

    created theme and dynamic whatsapp button

--- src/App.js
+++ src/App.js
@@ -14,8 +14,22 @@ import Header from './components/Header'
 import CookieConsent from 'react-cookie-consent'
 import Stef from './images/Stefano.jpeg'
 import {FloatingWhatsApp} from 'react-floating-whatsapp'
+import {createTheme, ThemeProvider} from '@mui/material/styles'
+import {useMediaQuery} from '@mui/material'
+
 
 function App() {
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+})
     const {t, i18n} = useTranslation()
     const [gigs, setGigs] = useState([])
     const [isStickyInitialized, setIsStickyInitialized] = useState(false)
@@ -23,6 +37,8 @@ function App() {
     // const URL = 'https://script.google.com/macros/s/AKfycbzCxuZAmM6xE0HNRcPMPgFTshmMU5zryez-jVPloxrg4c9VHo3CsuSFhmlfmySfeHOn/exec'
     const URL = 'https://script.google.com/macros/s/AKfycbwZpeq4v_TSnjAgento9RmcukZJtClAuSq5jmswlKnkJMGaXxsuPLXoQ2Eq-7f07cuW/exec'
     const selectedLanguage = i18n.language
+
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
     const getData = async () => {
         try {
             const response = await axios.get(URL)
@@ -50,44 +66,50 @@ function App() {
     const futureEvents = gigs.futureEvents || []
     const pastEvents = gigs.pastEvents || []
     const todayEvent = gigs.todayEvent || null
+
+
+    console.log(isMobile)
+
     return (
         <>
-            <CookieConsent
-                location="bottom"
-                buttonText={t('cookieStrip.OK')}
-                declineButtonText={t('cookieStrip.Decline')}
-                cookieName="myAwesomeCookieName2"
-                style={{background: '#2B373B'}}
-                buttonStyle={{color: '#4e503b', fontSize: 13}}
-                expires={150}
-                enableDeclineButton
-            >
-                {t('cookieStrip.Text')}
-            </CookieConsent>
-            <Fragment>
-                <main>
-                    <Header/>
-                    <NavBar/>
-                    <HeroSection/>
-                    <About/>
-                    <MeetTheBand/>
-                    <LiveGigs futureEvents={futureEvents} pastEvents={pastEvents} todayEvent={todayEvent}
-                              selectedLanguage={selectedLanguage} isLoading={isLoading}/>
-                    <Videos/>
-                </main>
-                <Footer currentYear={currentYear}/>
-            </Fragment>
-            <FloatingWhatsApp
-                phoneNumber="+393931518521"
-                accountName={'Stefano 🎸'}
-                statusMessage={t('whatsApp.repliesWithinHour')}
-                chatMessage={t('whatsApp.helloThere')}
-                placeholder={t('whatsApp.typeAMessage')}
-                buttonStyle={{transform: 'scale(0.8)', bottom: 10, right: 10}}
-                avatar={Stef}
-                allowClickAway
-                allowEsc
-            />
+            <ThemeProvider theme={theme}>
+                <CookieConsent
+                    location="bottom"
+                    buttonText={t('cookieStrip.OK')}
+                    declineButtonText={t('cookieStrip.Decline')}
+                    cookieName="myAwesomeCookieName2"
+                    style={{background: '#2B373B'}}
+                    buttonStyle={{color: '#4e503b', fontSize: 13}}
+                    expires={150}
+                    enableDeclineButton
+                >
+                    {t('cookieStrip.Text')}
+                </CookieConsent>
+                <Fragment>
+                    <main>
+                        <Header/>
+                        <NavBar/>
+                        <HeroSection/>
+                        <About/>
+                        <MeetTheBand/>
+                        <LiveGigs futureEvents={futureEvents} pastEvents={pastEvents} todayEvent={todayEvent}
+                                  selectedLanguage={selectedLanguage} isLoading={isLoading}/>
+                        <Videos/>
+                    </main>
+                    <Footer currentYear={currentYear}/>
+                </Fragment>
+                <FloatingWhatsApp
+                    phoneNumber="+393931518521"
+                    accountName={'Stefano 🎸'}
+                    statusMessage={t('whatsApp.repliesWithinHour')}
+                    chatMessage={t('whatsApp.helloThere')}
+                    placeholder={t('whatsApp.typeAMessage')}
+                    buttonStyle={{transform: 'scale(0.8)', bottom: isMobile ? 10 : 50, right: isMobile ? 10 : 50}}
+                    avatar={Stef}
+                    allowClickAway
+                    allowEsc
+                />
+            </ThemeProvider>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import i18n from './i18n';
-import { I18nextProvider } from 'react-i18next';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import i18n from './i18n'
+import {I18nextProvider} from 'react-i18next'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault(); // Prevents the default browser install prompt
+    event.preventDefault() // Prevents the default browser install prompt
     // Store the event for later use
-    window.deferredPrompt = event;
-});
+    window.deferredPrompt = event
+})
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <I18nextProvider i18n={i18n}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
     </I18nextProvider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
