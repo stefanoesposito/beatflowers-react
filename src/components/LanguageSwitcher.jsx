6fce61777b0e20b2e6dd398b148d11e518b8e4ce
Stefano Esposito <stefano.esposito@abstract.it>
2023-06-08 11:19:57 +0200

    flag icons

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import {Select, MenuItem} from '@mui/material'
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const supportedLanguages = ['en', 'it', 'fr', 'es', 'de', 'nl', 'pt', 'cs', 'sv', 'pl', 'ja', 'hi'];
 
@@ -14,18 +15,18 @@ function LanguageSwitcher() {
 
     return (
         <Select value={supportedLanguages.includes(i18n.language) ? i18n.language : 'en'} onChange={handleChange}>
-            <MenuItem value="it" title={'Italiano'}>&#x1F1EE;&#x1F1F9;</MenuItem>
-            <MenuItem value="en" title={'English'}>&#x1F1EC;&#x1F1E7;</MenuItem>
-            <MenuItem value="fr" title={'Français'}>&#x1F1EB;&#x1F1F7;</MenuItem>
-            <MenuItem value="es" title={'Español'}>&#x1F1EA;&#x1F1F8;</MenuItem>
-            <MenuItem value="de" title={'Deutsch'}>&#x1F1E9;&#x1F1EA;</MenuItem>
-            <MenuItem value="nl" title={'Nederlands'}>&#x1F1F3;&#x1F1F1;</MenuItem>
-            <MenuItem value="pt" title={'Português'}>&#x1F1E7;&#x1F1F7;</MenuItem>
-            <MenuItem value="cs" title={'čeština'}>&#x1F1E8;&#x1F1FF;</MenuItem>
-            <MenuItem value="sv" title={'Svenska'}>&#x1F1F8;&#x1F1EA;</MenuItem>
-            <MenuItem value="pl" title={'Polski'}>&#x1F1F5;&#x1F1F1;</MenuItem>
-            <MenuItem value="ja" title={'日本'}>&#x1F1EF;&#x1F1F5;</MenuItem>
-            <MenuItem value="hi" title={'हिंदी'}>&#x1F1EE;&#x1F1F3;</MenuItem>
+            <MenuItem value="it" title={'Italiano'}><span className="flag fi fi-it"></span></MenuItem>
+            <MenuItem value="en" title={'English'}><span className="flag fi fi-gb"></span></MenuItem>
+            <MenuItem value="fr" title={'Français'}><span className="flag fi fi-fr"></span></MenuItem>
+            <MenuItem value="es" title={'Español'}><span className="flag fi fi-es"></span></MenuItem>
+            <MenuItem value="de" title={'Deutsch'}><span className="flag fi fi-de"></span></MenuItem>
+            <MenuItem value="nl" title={'Nederlands'}><span className="flag fi fi-nl"></span></MenuItem>
+            <MenuItem value="pt" title={'Português'}><span className="flag fi fi-br"></span></MenuItem>
+            <MenuItem value="cs" title={'čeština'}><span className="flag fi fi-cz"></span></MenuItem>
+            <MenuItem value="sv" title={'Svenska'}><span className="flag fi fi-se"></span></MenuItem>
+            <MenuItem value="pl" title={'Polski'}><span className="flag fi fi-pl"></span></MenuItem>
+            <MenuItem value="ja" title={'日本'}><span className="flag fi fi-jp"></span></MenuItem>
+            <MenuItem value="hi" title={'हिंदी'}><span className="flag fi fi-in"></span></MenuItem>
         </Select>
     );
 }
