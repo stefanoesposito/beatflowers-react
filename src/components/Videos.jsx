abe7bab5558ad39e2d3d1611c4bf2119554013ed
Stefano Esposito <stefano.esposito@abstract.it>
2023-06-12 09:39:34 +0200

    video reorder and titles

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -12,6 +12,7 @@ const Videos = () => {
                         <h2 className="text-center mb-4">{t('menu.Videos')}</h2>
                     </div>
 
+                    {/* Nowhere Man */}
                     <div className="col-lg-6 col-12 mb-lg-4 mt-lg-0">
                         <iframe
                             width="100%"
@@ -24,30 +25,7 @@ const Videos = () => {
                         ></iframe>
                     </div>
 
-                    <div className="col-lg-6 col-12 mt-4 mb-lg-4 mt-lg-0">
-                        <iframe
-                            width="100%"
-                            height="315"
-                            src="https://www.youtube.com/embed/-U0Eqo_ZbpU"
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-
-                    <div className="col-lg-6 col-12 mb-lg-4 mt-4 mt-lg-0">
-                        <iframe
-                            width="100%"
-                            height="315"
-                            src="https://www.youtube.com/embed/VdVE4My-MsA"
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-
+                    {/* Yesterday */}
                     <div className="col-lg-6 col-12 mb-lg-4 mt-4 mt-lg-0">
                         <iframe
                             width="100%"
@@ -60,6 +38,33 @@ const Videos = () => {
                         ></iframe>
                     </div>
 
+                    {/* Kestè */}
+                    <div className="col-lg-6 col-12 mt-4 mb-lg-4 mt-lg-0">
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="https://www.youtube.com/embed/-U0Eqo_ZbpU"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+
+                    {/* Penny Lane */}
+                    <div className="col-lg-6 col-12 mb-lg-4 mt-4 mt-lg-0">
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="https://www.youtube.com/embed/VdVE4My-MsA"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+
+                    {/* JAM Cave */}
                     <div className="col-lg-6 col-12 mb-lg-4 mt-4 mt-lg-0">
                         <iframe
                             width="100%"
@@ -72,6 +77,7 @@ const Videos = () => {
                         ></iframe>
                     </div>
 
+                    {/* I'm Only Sleeping */}
                     <div className="col-lg-6 col-12 mb-lg-4 mt-4 mt-lg-0">
                         <iframe
                             width="100%"
